177aa70dbe0b251201572d35a9aef0952dec3f77
GoodOldBob <bobfan03@gmail.com>
2017-01-03 21:43:25 -0800

    hackerrank login

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import urllib
 import getpass
-
+import time
 """
 Before using, make sure the ChromeDriver executable is in your PATH.
 """
@@ -14,13 +14,16 @@ def login_hackerrank(driver):
     login_name = input("Enter your username: ")
     login_pass = getpass.getpass("Enter your password: ")
 
-    username = driver.find_element_by_id("login")
-    password = driver.find_element_by_id("password")
-    username.send_keys(login_name)
-    password.send_keys(login_pass)
-    driver.find_element_by_name("commit").click()
+    driver.get(HACKERRANK_LOGIN_URL)
+    driver.find_element_by_xpath("//input[@data-attr1='UserName']").send_keys(login_name)
+    #time.sleep(10)
+    driver.find_element_by_xpath("//input[@data-attr1='UserName' and @id='password']").send_keys(login_pass)
+    driver.find_element_by_xpath("//*[@id='legacy-login']/div[1]/p/button").click()
+    driver.save_screenshot("out.png")
+
 
 driver = webdriver.PhantomJS()
+#driver.set_window_size(1024, 768)
 login_hackerrank(driver)
 url = input("Enter the problem url to scrape: ")
 driver.get(url)
